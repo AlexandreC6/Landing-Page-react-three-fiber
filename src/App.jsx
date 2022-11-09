379cfe7e62200b7a2a76fd9cc167dcb5f6822576
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-11-09 21:16:22 +0100

    first render with animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
 import * as THREE from 'three'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { useRef, useState } from 'react'
 
-function Box() {
-  const [clicked, setClicked] = useState(false)
+function Box({ z }) {
 
   const ref = useRef();
+  const {viewport, camera} = useThree()
+  const {width, height} = viewport.getCurrentViewport(camera, [0, 0, z])
+
+
+  const [data] = useState({
+    x: THREE.MathUtils.randFloatSpread(2),
+    y: THREE.MathUtils.randFloatSpread(height),
+  })
+
   useFrame((state) => {
-    ref.current.position.z = THREE.MathUtils.lerp(ref.current.position.z, clicked ? 3 : 0, 0.2)
+    ref.current.position.set(data.x * width, (data.y += 0.5), z);
+    if (data.y >  height / 1.5) {
+      data.y = - height / 1.5
+    }
   })
 
   return (
-    <mesh ref={ref} onClick={() => {setClicked(!clicked)}}>
+    <mesh ref={ref}>
       <boxGeometry />
       <meshBasicMaterial color="red" />
     </mesh>
   )
 }
 
-export default function App() {
+function Test() {
+  const {viewport, camera} = useThree()
+  const {width, height} = viewport.getCurrentViewport(camera, [0, 0, 0])
+
+  return <mesh position={[0, 0, -10]} scale={[viewport.width, viewport.height, 1]}>
+    <planeGeometry />
+    <meshBasicMaterial color="orange"/>
+  </mesh>
+}
+
+export default function App({count = 100}) {
   return (
     <Canvas>
-      <Box />
+      {Array.from({length: count}, (_,i) => (<Box key={i} z={-i}/>))}
+      {/* <Test /> */}
     </Canvas>
   )
 }
